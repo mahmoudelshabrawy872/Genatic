caac72e919d1a23715f80e8e9ea79cc4f7f4cb60
Mahmoud El Shabrawy <Mahmoudalshabrawy@outlook.com>
2023-12-26 06:10:37 +0200

    ###

---
+++ main.py
@@ -0,0 +1,127 @@
+# 1. Leather Limited manufactures two types of leather belts: the deluxe model and the regular model. Each type
+# requires 1 square yard of leather. A regular belt requires 1 hour of skilled labor and a deluxe belt requires 2
+# hours of skilled labor. Each week, 40 square yards of leather and 60 hours of skilled labor are available. Each
+# deluxe belt contributes $4 profit and each regular belt contributes $3 profit. The LP that maximizes profit
+
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import geneticalgorithm as ga
+
+# Define the objective function
+def objective_function(x1, x2):
+    return 4 * x1 + 3 * x2
+
+
+# Define Constraints
+def constraint1(x1, x2):
+    return x1 + x2 <= 40
+
+
+def constraint2(x1, x2):
+    return 2 * x1 + x2 <= 60
+
+#Genetic Algorithm Function
+def genetic_algorithm(population_size, num_generation):
+    # Initialization
+    population = []
+    fitness_history = []
+
+    for i in range(population_size):
+        x1 = random.uniform(0, 40)
+        x2 = random.uniform(0, 60)
+        population.append((x1, x2))
+
+    best_solution = None
+    best_fitness = float('-inf')
+
+    for generation in range(num_generation):
+        # Evaluation
+        fitness = [objective_function(x1, x2) for x1, x2 in population]
+
+        feasible_population = [individual for individual in population if
+                               constraint1(individual[0], individual[1]) and constraint2(individual[0], individual[1])]
+
+        # Selection
+        if feasible_population:
+            feasible_fitness = [objective_function(x1, x2) for x1, x2 in feasible_population]
+            parents = random.choices(feasible_population, weights=feasible_fitness, k=population_size)
+        else:
+            parents = []
+            while len(parents) < population_size:
+                potential_parents = random.choice(population, weights=fitness)
+                if constraint1(potential_parents[0], potential_parents[1]) and constraint2(potential_parents[0],
+                                                                                           potential_parents[1]):
+                    parents.append(potential_parents)
+
+        # Crossover
+        offspring = []
+        for i in range(population_size):
+            parent1, parent2 = random.choices(parents, k=2)
+            x1_child = random.uniform(min(parent1[0], parent2[0]), max(parent1[0], parent2[0]))
+            x2_child = random.uniform(min(parent1[1], parent2[1]), max(parent1[1], parent2[1]))
+            offspring.append((x1_child, x2_child))
+        #########################
+        # Parent1: (x1=10 , x2=5)
+        # Parent2: (x1=20 , x2=15)
+        # The minimum value is min(parent1[0],parent2[0]) = min(10,20) = 10
+        # The Maximum value is max(parent1[0],parent2[0]) = max(10,20) = 20
+        # random.uniform(10,20)    x1_child = 17
+        # The minimum value is min(parent1[1],parent2[1]) = min(5,15) = 5
+        # The Maximum value is max(parent1[1],parent2[1]) = max(5,15) = 15
+        # random.uniform(5,15)    x2_child = 11
+        # offspring: (x1=17,x2=11)
+        #########################
+
+        # Mutation
+        mutation_rate = 1 / (generation + 1)  # Dynamic mutation rate
+        for i in range(population_size):
+            if random.random() < mutation_rate:
+                offspring[i] = (random.uniform(0, 40), random.uniform(0, 60))
+
+        # mutation_rate = 1/(1+1) = 0.5
+        # random.random() = 0.75
+        # No mutations occur
+
+        # random.random() = 0.2 < mutation_rate(0.5)
+        # New values for the offspring after mutation (new_x1, new_x2)
+
+        # Beginning - Exploration - High mutation rate
+        # Later generations - Exploitation - Low mutation rate
+
+        # Elitism
+        if best_solution is not None:
+            offspring[0] = best_solution
+
+        population = offspring
+
+        # Find the best feasible solution
+        feasible_solutions = [(x1, x2) for (x1, x2) in population if constraint1(x1, x2) and constraint2(x1, x2)]
+        if feasible_solutions:
+            best_solution = max(feasible_solutions, key=lambda x: objective_function(x[0], x[1]))
+            best_fitness = objective_function(best_solution[0], best_solution[1])
+        fitness_history.append(best_fitness)
+
+        print(f"Generation{generation + 1}: Best solution = {best_solution},Best fitness = {best_fitness}")
+
+    # Plot the fitness progress
+    plt.plot(range(1, num_generation + 1), fitness_history)
+    plt.xlabel("Generation")
+    plt.ylabel("Best Fitness")
+    plt.title("GA - Fitness Progress")
+    plt.show()
+
+    return best_solution, best_fitness
+
+
+# Call the GA Function
+population_size = 10000
+num_generation = 50
+
+best_solution, best_fitness = genetic_algorithm(population_size, num_generation)
+
+if best_solution is not None:
+    print("Final best solution:", best_solution)
+    print("Final best fitness:", best_fitness)
+else:
+    print("No feasible solution found within the given constraints")
---
+++ test.py
@@ -0,0 +1,50 @@
+import numpy as np
+from geneticalgorithm import geneticalgorithm as ga
+
+def fitness_function(X):
+    x = X[0]
+    y = X[1]
+
+    leather_used = x + y
+    labor_used = x + 2 * y
+
+    # Check constraints
+    if leather_used > 40 or labor_used > 60 or x < 0 or y < 0:
+        penalty = np.inf
+    else:
+        penalty = 0
+
+    profit = 3 * x + 4 * y - penalty
+
+    return -profit  # Minimize negative profit
+
+algorithm_params = {
+    'max_num_iteration': None,
+    'population_size': 100,
+    'mutation_probability': 0.4,  # Adjust as needed
+    'elit_ratio': 0.01,
+    'crossover_probability': 0.5,
+    'parents_portion': 0.3,
+    'crossover_type': 'uniform',
+    'max_iteration_without_improv': None
+}
+
+# Convert variable_boundaries to a NumPy array
+variable_boundaries = np.array([(0, 40), (0, 40)])
+
+model = ga(
+    function=fitness_function,
+    dimension=2,  # Two decision variables: x and y
+    variable_type='int',  # Decision variables are integers
+    variable_boundaries=variable_boundaries,  # Use the NumPy array
+    algorithm_parameters=algorithm_params
+)
+
+model.run()
+
+# Display the results
+best_solution = model.output_dict['variable']
+max_profit = -model.output_dict['function']
+
+print("Best Solution:", best_solution)
+print("Max Profit:", max_profit)
